30bfd8d64974898750c40937861bf30d8c9e898c
zehratunc <zehra24tunc@outlook.com>
2024-11-10 18:39:40 +0300

    complete cookie, add a favorite button

---
+++ cookies.js
@@ -0,0 +1,33 @@
+$(document).ready(function() { 
+    let task = $("#task_input").val().trim();
+
+    let data = [];
+
+    $('#save-button').on('click', () => {
+        console.log('zeyram');
+    });
+
+    // Çerezi oluşturma fonksiyonu
+    // function setCookie(name, value, days) {
+    //     let expires = "";
+    //     if (days) {
+    //         const date = new Date();
+    //         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    //         expires = "; expires=" + date.toUTCString();
+    //     }
+    //     document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    // }
+
+    // // Çerezi okuma fonksiyonu
+    // function getCookie(name) {
+    //     const nameEQ = name + "=";
+    //     const ca = document.cookie.split(';');
+    //     for(let i = 0; i < ca.length; i++) {
+    //         let c = ca[i];
+    //         while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    //         if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    //     }
+    //     return null;
+    // }
+
+});
